01fd2c91bc5c587e6b80bb05ffd09f5c60579ae4
joshua1996 <kjw1996@hotmail.com>
2018-01-11 20:35:24 +0800

    finish home page

--- js/my-app.js
+++ js/my-app.js
@@ -1,4 +1,5 @@
 var app = new Framework7({
+    init: false,
     // App root element
     root: '#app',
     // App Name
@@ -13,35 +14,45 @@ var app = new Framework7({
     routes: [{
         path: '/home/',
         url: 'home.html',
+    }, {
+        path: '/',
+        url: 'index.html',
     }],
-    // ... other parameters
+
 });
 
 var $$ = Dom7;
 
 var mainView = app.views.create('.view-main', {
-    url: '/'
+});
+
+$$(document).on('page:init', '.page[data-name="intro"]', function (e) {
+    if (localStorage.getItem('skipIntro') === null) {
+
+    } else {
+        $$('.page[data-name="intro"]').remove();
+         mainView.router.navigate('/home/');
+    }
+});
+
+app.init();
+
+$$(document).on('click', '#getStartBtn', function() {
+    localStorage.setItem('skipIntro', true);
+    
+});
+
+$$(document).on('page:afterout', '.page[data-name="intro"]', function (e) {
+    $$('.page[data-name="intro"]').remove();
+});
+
+$$(document).on('page:init', '.page[data-name="home"]', function (e) {
 });
 
 
-
-
-  $$(document).on('page:init', '.page[data-name="home"]', function (e) {
-      console.log('htr');
-    //mainView.destroy();
-  })
-
-  $$(document).on('click', '.abc', function (e){
-    mainView.destroy();
-  })
 var swiper = app.swiper.create('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
     },
-    on: {
-        progress: function (){
-           console.log('gerg');
-        }
-    }
 });
